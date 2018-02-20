683555ad4cd1bd27810f2f45a796b4b919fb5d57
CosmiCat <mimoro1176@hotmail.com>
2018-02-20 17:06:09 -0500

    INotifyPropertyChanged with the BaseViewModel

--- otherLands/otherLands/ViewModels/LoginViewModel.cs
+++ otherLands/otherLands/ViewModels/LoginViewModel.cs
@@ -1,11 +1,18 @@
 ﻿namespace otherLands.ViewModels
 {
     using GalaSoft.MvvmLight.Command;
-    using System;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
+
+        #region Attributes
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
         #region Properties
         public string Email
         {
@@ -15,14 +22,14 @@
 
         public string Password
         {
-            get;
-            set;
+            get { return password; }
+            set { SetValue(ref password, value);  }
         }
 
         public bool IsRunning
         {
-            get;
-            set;
+            get { return isRunning; }
+            set { SetValue(ref isRunning, value); }
         }
 
         public bool IsRemembered
@@ -30,12 +37,19 @@
             get;
             set;
         }
+
+        public bool IsEnabled
+        {
+            get { return isEnabled; }
+            set { SetValue(ref isEnabled, value); }
+        }
         #endregion
 
         #region Constructors
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnabled = true;
         }
         #endregion
 
@@ -48,9 +62,50 @@
             }
         }
 
-        private void Login()
+        private async void Login()
         {
+           if(string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error", 
+                    "You must enter an Email", 
+                    "Accept");
+                return;
+            }
 
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a Password",
+                    "Accept");
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email != "m@tcs.com" || this.Password != "123")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password incorret.",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "OK",
+                    "Naisa!!",
+                    "Accept");
+            return;
         }
         #endregion
     }
