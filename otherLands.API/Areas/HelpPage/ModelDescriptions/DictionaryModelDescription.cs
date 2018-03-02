77ec1d7a4fbc43f264a67cdf9f5fdcc596266aea
CosmiCat <mimoro1176@hotmail.com>
2018-03-02 14:22:44 -0500

    BackEnd Projects
    Create projects: API, BackEnd and Domain
    ConnectionStrings
    IdentityConfig

---
+++ otherLands.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace otherLands.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ otherLands.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="otherLands.API.WebApiApplication" Language="C#" %>
